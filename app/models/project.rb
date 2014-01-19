80086e497ffc94903df4b12f148b0f9d829b98f4
Wojciech Ogrodowczyk <wojtek@7tonlnu.pl>
2014-01-19 21:21:07 +0100

    Add project methods: #items_by_days and #recent_items.

--- app/models/project.rb
+++ app/models/project.rb
@@ -6,4 +6,17 @@ class Project < ActiveRecord::Base
   def done?
     items.where(completed_at: nil).count == 0
   end
+
+  def items_by_days
+    result = Hash.new([])
+    items.each do |item|
+      result[item.created_at.to_date] += [item]
+    end
+
+    result
+  end
+
+  def recent_items
+    items.where ['created_at > ?', Time.now.beginning_of_day]
+  end
 end
--- spec/models/project_spec.rb
+++ spec/models/project_spec.rb
@@ -28,4 +28,44 @@ describe Project do
       project.done?.should == false
     end
   end
+
+  describe '#items_by_days' do
+    describe 'with no tasks' do
+      it 'returns an empty hash' do
+        project.items_by_days.should == {}
+      end
+    end
+
+    describe 'when tasks exist' do
+      before do
+        FactoryGirl.create(:task, created_at: DateTime.parse('1983-11-27 13:45'), project: project)
+        FactoryGirl.create(:task, created_at: DateTime.parse('1983-11-27 15:45'), project: project)
+        FactoryGirl.create(:task, created_at: DateTime.parse('1983-12-05 9:15'), project: project)
+      end
+
+      it 'returns a result grouped by days' do
+        project.items_by_days[Date.parse('1983-11-27')].size.should == 2
+      end
+
+      it 'returns grouped tasks' do
+        project.items_by_days[Date.parse('1983-12-05')].first.should be_a Task
+      end
+
+      it 'returns a hash' do
+        project.items_by_days.should be_a Hash
+      end
+    end
+  end
+
+  describe '#recent_items' do
+    before do
+      @task1 = FactoryGirl.create(:task, created_at: Time.now.beginning_of_day + 5.hours, project: project)
+      @task2 = FactoryGirl.create(:task, created_at: Time.now.beginning_of_day - 5.hours, project: project)
+    end
+
+    it 'returns only items from today' do
+      project.recent_items.size.should == 1
+      project.recent_items.first.should == @task1
+    end
+  end
 end

f26150b746bf6965b99628e4f51e17e0ce560286
Wojciech Ogrodowczyk <wojtek@7tonlnu.pl>
2014-01-20 15:15:33 +0100

    Separate open and closed tasks.

--- app/assets/javascripts/react/tasks/task_list.js.jsx
+++ app/assets/javascripts/react/tasks/task_list.js.jsx
@@ -1,9 +1,22 @@
 /** @jsx React.DOM */
 var TaskList = React.createClass({
-  render: function() {
+  getOpenTasks: function() {
+    return this.props.data.filter(function(el) {
+      return el.completed_at == null
+    });
+  },
+
+  getClosedTasks: function() {
+    return this.props.data.filter(function(el) {
+      return !(el.completed_at == null)
+    });
+  },
+
+  mapToTask: function(array) {
     var completeHandler = this.props.onTaskComplete;
     var openHandler = this.props.onTaskOpen;
-    var taskNodes = this.props.data.map(function (task) {
+
+    return array.map(function (task) {
       return <Task
         completed_at={task.completed_at}
         id={task.id}
@@ -14,9 +27,15 @@ var TaskList = React.createClass({
           {task.content}
         </Task>;
     });
+  },
+
+  render: function() {
+    var openTaskNodes = this.mapToTask(this.getOpenTasks());
+    var closedTaskNodes = this.mapToTask(this.getClosedTasks());
     return (
       <div>
-        {taskNodes}
+        <div className='open-tasks'>{openTaskNodes}</div>
+        <div className='closed-tasks'>{closedTaskNodes}</div>
       </div>
     );
   }
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,9 @@
 p = Project.create!(name: 'Dog stuff', description: 'Just some stuff a dog does')
 
-Task.create!(project: p, content: 'Eat')
+Task.create!(project: p, content: 'Eat', completed_at: 3.days.ago)
 Task.create!(project: p, content: 'Sleep')
 Task.create!(project: p, content: 'Wag the tail')
 
 p = Project.create!(name: 'Cat stuff', description: 'Just some stuff a dog would never do')
 Task.create!(project: p, content: 'Meow')
-Task.create!(project: p, content: 'Pee on the lemon tree')
+Task.create!(project: p, content: 'Pee on the lemon tree', completed_at: 3.hours.ago)

d12b97ffb01dfcd23944ec2c976216587ba34b29
Wojciech Ogrodowczyk <wojtek@7tonlnu.pl>
2014-01-20 11:03:44 +0100

    Display tasks from JSON.

--- app/assets/javascripts/init.js.jsx
+++ app/assets/javascripts/init.js.jsx
@@ -1,11 +1,5 @@
 /** @jsx React.DOM */
-var data = [
-  {content: "Eat", id: 1, completed_at: '2013-01-10 11:30'},
-  {content: "Sleep", id: 2, completed_at: '2013-01-10 13:45'},
-  {content: "Wag the tail", id: 3, completed_at: null}
-];
-
 React.renderComponent(
-  <TaskBox data={data} />,
+  <TaskBox url='tasks.json' pollInterval={2500} />,
   document.getElementById('task-content')
 );
--- app/assets/javascripts/react/tasks/task_box.js.jsx
+++ app/assets/javascripts/react/tasks/task_box.js.jsx
@@ -1,10 +1,32 @@
 /** @jsx React.DOM */
 var TaskBox = React.createClass({
+  getInitialState: function() {
+    return {data: []};
+  },
+
+  loadTasksFromServer: function() {
+    $.ajax({
+      url: 'tasks.json',
+      dataType: 'json',
+      success: function(data) {
+        this.setState({data: data});
+      }.bind(this),
+      error: function(xhr, status, err) {
+        console.error("tasks.json", status, err.toString());
+      }.bind(this)
+    });
+  },
+
+  componentWillMount: function() {
+    this.loadTasksFromServer();
+    setInterval(this.loadTasksFromServer, this.props.pollInterval);
+  },
+
   render: function() {
     return (
       <div className="task-list well">
         <h3>This is what you need to do:</h3>
-        <TaskList data={this.props.data}/>
+        <TaskList data={this.state.data}/>
         <TaskForm />
       </div>
     );
